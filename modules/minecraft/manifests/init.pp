e7528e01574ee9c3d366e044be2a3b598c63baa8
narkosha <narko2t1@gmail.com>
2023-07-15 18:31:03 +0300

    fix init

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -11,7 +11,8 @@ class minecraft {
   file { '/opt/minecraft/minecraft_server.1.20.1.jar':
   ensure => present,
   source => 'https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar',
-
+  }
+  
   file { '/opt/minecraft/server.properties':
     ensure => file,
   }
