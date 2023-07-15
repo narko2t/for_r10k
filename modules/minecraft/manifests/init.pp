87938c25e444ee099cd0219a810924cda0284bb9
narkosha <narko2t1@gmail.com>
2023-07-15 17:43:50 +0300

    update init.pp

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -11,13 +11,7 @@ class minecraft {
   exec { 'download_minecraft_server':
     command     => 'wget https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar -O minecraft_server.jar',
     cwd         => '/opt/minecraft',
-    refreshonly => true,
-  }
-
-  exec { 'install_minecraft_server':
-    command     => 'java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui',
-    cwd         => '/opt/minecraft',
-    creates     => '/opt/minecraft/server.properties',
+    creates     => '/opt/minecraft/server.properties'
     refreshonly => true,
   }
 
